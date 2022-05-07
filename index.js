f82c863bfd0c18b7aef298f719446239c8673d98
manalabePatrick <manalabepatrick@gmail.com>
2022-05-07 12:29:33 +0800

    updated

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "gcap",
+  multipleStatements: true,
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("succesfully connected to the database");
+});
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
